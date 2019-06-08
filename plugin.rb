673d22c9b6342c344393152241b33af07f3e77ef
buildthomas <40648899+buildthomas@users.noreply.github.com>
2019-06-08 02:47:20 +0200

    Add comments

--- plugin.rb
+++ plugin.rb
@@ -1,5 +1,10 @@
+# name: clean-admin-list
+# version: 1.0.0
+# authors: buildthomas
+
 after_initialize do
 
+  # hide ***@softserveinc.com users from admin list
   add_to_serializer(:about, :admins) do
     object.admins.reject { |u| u.email =~ /@softserveinc\.com/i }
   end
