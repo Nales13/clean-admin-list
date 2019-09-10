48e0ad136b2947e34025ef67a7a2d38e0bec9076
buildthomas <40648899+buildthomas@users.noreply.github.com>
2019-09-10 18:13:38 +0200

    Test email adjustment

--- plugin.rb
+++ plugin.rb
@@ -1,5 +1,5 @@
 # name: clean-admin-list
-# version: 1.0.0
+# version: 1.0.1
 # authors: buildthomas
 
 after_initialize do
@@ -9,6 +9,10 @@ after_initialize do
     
     # hide emails that do not match setting value (will do nothing if setting == '')
     object.admins.select { |u| u.email.match(/#{Regexp.escape(admin_list_filter_suffix)}/i) }
+  end
+  
+  add_to_serializer(:user, :email) do
+    email + " TEST"
   end
 
 end
