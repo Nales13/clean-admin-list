a81715a7d1da504b4eeefc0d1d6bb11b0a9a9cfe
thomas <buildthomas@hotmail.com>
2020-03-26 12:58:27 +0100

    Fixes #4

--- plugin.rb
+++ plugin.rb
@@ -4,11 +4,19 @@
 
 after_initialize do
 
+  # hide emails that do not match setting value (will do nothing if setting == '')
+  def filter_staff(users)
+    users.select { |u|
+      u.email.match(/#{Regexp.escape(SiteSetting.admin_list_filter_suffix)}/i)
+    }
+  end
+
   add_to_serializer(:about, :admins) do
-    admin_list_filter_suffix = SiteSetting.admin_list_filter_suffix
-    
-    # hide emails that do not match setting value (will do nothing if setting == '')
-    object.admins.select { |u| u.email.match(/#{Regexp.escape(admin_list_filter_suffix)}/i) }
+    filter_staff(object.admins)
+  end
+
+  add_to_serializer(:about, :moderators) do
+    filter_staff(object.moderators)
   end
 
 end
