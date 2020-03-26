badc4e8d43e704991559774a2b59278099de68c5
thomas <buildthomas@hotmail.com>
2020-03-26 13:03:12 +0100

    Fixes #3

--- plugin.rb
+++ plugin.rb
@@ -2,10 +2,14 @@
 # version: 1.0.0
 # authors: buildthomas
 
+enabled_site_setting :clean_admin_list_enabled
+
 after_initialize do
 
-  # hide emails that do not match setting value (will do nothing if setting == '')
   def filter_staff(users)
+    return users unless SiteSetting.clean_admin_list_enabled
+    
+    # hide emails that do not match setting value (will do nothing if setting == '')
     users.select { |u|
       u.email.match(/#{Regexp.escape(SiteSetting.clean_admin_list_suffix)}/i)
     }
